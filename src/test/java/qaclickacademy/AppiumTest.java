09a091ce9049dbc1baa6ca809966098ce94f1c58
ravish523 <ravishkr87@gmail.com>
2020-01-27 23:54:52 +0530

    first commit of my project

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void Browser()
+	{
+		System.out.println("abcsdlkjhhhhdhh");
+	}
+
+	@Test
+	public void elements()
+	{
+		System.out.println("abcsdlkjhhhn");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/RestApiTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+	@Test
+	public void Automation()
+	{
+		System.out.println("abcsdlkjhhhhdhhdhn");
+	}
+
+	@Test
+	public void ementsUi()
+	{
+		System.out.println("csdlkjhhhhdhhdhn");	
+	}
+
+}
